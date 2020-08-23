9bed95a759c6342c92326323a2c1cbf51a670840
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2020-08-23 20:54:11 +0300

    3.4 tehty

--- index.js
+++ index.js
@@ -47,6 +47,13 @@ let persons = [
     }
   })
 
+  app.delete('/api/persons/:id', (request, response) =>{
+      const id = Number(request.params.id)
+      persons = persons.filter(person => person.id !== id)
+
+      response.status(204).end()
+  })
+
 const port = 3001
 app.listen(port)
 console.log(`Server running on port ${port}`)
