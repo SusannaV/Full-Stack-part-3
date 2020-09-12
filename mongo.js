a6a558ec05bc5fc883e2a16b2f25eb25876e0be6
Susanna Veijalainen <susanna.veijalainen@gmail.com>
2020-09-12 14:29:57 +0300

    3.12 tehty

---
+++ mongo.js
@@ -0,0 +1,46 @@
+const mongoose = require('mongoose')
+
+// if (process.argv.length<3) {
+//     console.log('give password as argument')
+//     process.exit(1)
+//   }
+  
+  const password = process.argv[2]
+  
+  const url =
+    
+     `mongodb+srv://fullstackuser:${password}@puhelinluettelocluster.vg2su.mongodb.net/puhelinluettelo?retryWrites=true&w=majority`
+  mongoose.connect(url, { useNewUrlParser: true, useUnifiedTopology: true })
+  
+  const personSchema = new mongoose.Schema({
+    name: String,
+    number: String,
+    //id: Number,
+  })
+  
+  const Person = mongoose.model('Person', personSchema)
+  
+  if (process.argv.length > 3) {
+  const person = new Person({
+    name: process.argv[3],
+    number: process.argv[4],
+    //id: 1,
+  })
+  
+  person.save().then(response => {
+    console.log('person saved!')
+    mongoose.connection.close()
+  })
+
+
+  } else {
+    console.log('Phonebook:')  
+    Person.find({  }).then(result => {
+    result.forEach(person => {
+      console.log(person.name, person.number)
+    })
+    mongoose.connection.close()
+  })
+}
+
+  
